fb0ce685cd4e6bc2998fa8a5d56a906129c96515
masato <masato.ngsm@gmail.com>
2022-04-14 04:30:35 +0900

    fix: bug
            何度もログが出力されるのを修正
    
            authored-by: hoge

--- packages/angular-parser/src/list-invalid-attr.ts
+++ packages/angular-parser/src/list-invalid-attr.ts
@@ -4,82 +4,94 @@ import path from 'path'
 import { exit } from 'process'
 import readline from 'readline'
 
-const canBeDirectiveFile = (file: string) =>
-  file.includes('.ts') && !file.includes('.spec.ts') && !file.includes('.d.ts')
+class DirectoryWalker {
+  private readonly directories: string[] = []
+  private readonly directiveSelectors: string[] = []
 
-const directiveSelectorLines: string[] = []
-
-const collectSelector = (eachLine: string) => {
-  if (eachLine.includes('selector') && !eachLine.includes('-')) {
-    directiveSelectorLines.push(eachLine)
-  }
-}
-
-const showResult = () => {
-  const directiveSelectors = directiveSelectorLines.map(
-    (line: string, index: number) =>
-      `${line
-        .split(':')[1]
-        .replace('[', '')
-        .replace(']', '')
-        .replace(',', '')}${
-        index === directiveSelectorLines.length - 1 ? '' : ','
-      }`,
-  )
-
-  console.log(`Add the following to your markuplintrc
-{
-  'invalid-attr': {
-    'option': {
-      'ignoreAttrNamePrefix': [
-        'app',
-        '*ng',
-        'ng',
-        '(',
-        '[',
-        '@',
-        '#',`)
-  for (const selector of directiveSelectors) {
-    console.log(`       ${selector}`)
-  }
-  console.log(`      ]
-    }
-  }
-}  
-  `)
-}
-
-const checkSelector = (possibleDirectiveFile: string) => {
-  const readStream = fs.createReadStream(possibleDirectiveFile)
-  const readlineInterface = readline.createInterface({
-    input: readStream,
-    output: undefined,
-  })
-  readlineInterface.on('line', collectSelector)
-  readlineInterface.on('close', showResult)
-}
-
-const checkDirectory = (dir: string) => {
-  fs.readdir(dir, (err, files) => {
-    if (err) {
-      console.error(err)
-      exit(2)
+  walk(directory: string): void {
+    this.searchDirectoryForDirectiveSelector(directory)
+    const hasFinishedWalking = this.directories.length === 0
+    if (hasFinishedWalking) {
+      this.printResult()
     } else {
-      for (const file of files) {
-        const filePath = path.join(dir, file)
-        const stats = fs.statSync(filePath)
-        if (stats.isFile()) {
-          if (canBeDirectiveFile(filePath)) {
-            checkSelector(filePath)
-          }
-        } else {
-          checkDirectory(filePath)
-        }
+      for (const nextDirectory of this.directories) {
+        this.walk(nextDirectory)
       }
     }
-  })
+  }
+
+  private searchDirectoryForDirectiveSelector(directory: string): void {
+    fs.readdir(directory, (err, files) => {
+      if (err) {
+        console.error(err)
+        exit(2)
+      } else {
+        for (const file of files) {
+          const filePath = path.join(directory, file)
+          const stats = fs.statSync(filePath)
+          if (stats.isFile()) {
+            this.seachFileForDirectiveSelector(filePath)
+          } else {
+            this.directories.push(filePath)
+          }
+        }
+      }
+    })
+  }
+
+  private seachFileForDirectiveSelector(filePath: string) {
+    const readStream = fs.createReadStream(filePath)
+    const readlineInterface = readline.createInterface({
+      input: readStream,
+      output: undefined,
+    })
+    readlineInterface.on('line', line => this.collectDirectiveSelector(line))
+  }
+
+  private collectDirectiveSelector(line: string) {
+    const isDirectiveSelectorLine = line.includes('selector')
+    const isComponentSelectorLine =
+      isDirectiveSelectorLine && line.includes('-')
+
+    if (isDirectiveSelectorLine && !isComponentSelectorLine) {
+      this.directiveSelectors.push(this.extractDirectiveSelector(line))
+    }
+  }
+
+  private extractDirectiveSelector(directiveSelectorLine: string): string {
+    // extract "directiveName" from "selector:[directiveName],"
+    return directiveSelectorLine
+      .split(':')[1]
+      .replace('[', '')
+      .replace(']', '')
+      .replace(',', '')
+  }
+
+  private printResult(): void {
+    console.log(`Add the following to your markuplintrc
+  {
+    'invalid-attr': {
+      'option': {
+        'ignoreAttrNamePrefix': [
+          'app',
+          '*ng',
+          'ng',
+          '(',
+          '[',
+          '@',
+          '#',`)
+    for (const [index, selector] of this.directiveSelectors.entries()) {
+      const isLastDirective = index === this.directiveSelectors.length - 1
+      console.log(`       ${selector}${isLastDirective ? '' : ','}`)
+    }
+    console.log(`      ]
+      }
+    }
+  }  
+    `)
+  }
 }
 
 // Not all Angular projects put files under src directory(e.g. monorepo app, library, .etc)
 const srcDir = process.argv[2] || 'src'
-checkDirectory(srcDir)
+new DirectoryWalker().walk(srcDir)
